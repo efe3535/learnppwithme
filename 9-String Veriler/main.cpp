13f8a667e1fff0cbaeb13d614b5562a8a4325b3b
inoaa79 <inoaa@inoaa.space>
2022-01-13 20:58:42 +0300

    Codes of my first lesson

--- 9-String Veriler/main.cpp
+++ 9-String Veriler/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "INOAA79" << endl;
+    string isim;
+    string isim2;
+    string soyisim;
+    string meslek;
+    string meslek2;
+    int yas;
+
+    cout<<"Lutfen isminizi girin."<<endl;
+    cin >> isim;
+    cin >> isim2;
+    cout<<"Lutfen soyadinizi girin"<<endl;
+    cin >> soyisim;
+    cout<<"Lutfen yasinizi girin"<<endl;
+    cin >> yas;
+    cout<<"Lutfen mesleginizi girin"<<endl;
+    cin >>meslek;
+    cin >> meslek2;
+    cout<<"--Kullanıcı bilgileri--"<<endl<<"İsim:"<<isim<<" "<<isim2<<endl<<"Soyisim:"<<soyisim<<endl<<"Yaş:"<<yas<<endl<<"Meslek:"<<meslek<<" "<<meslek2<<endl;
     return 0;
 }
